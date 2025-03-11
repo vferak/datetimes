d0529ab65425997ca8ee32bb4c84dc9e27420808
Vojtěch Ferák <vferak@gmail.com>
2025-03-11 21:31:54 +0100

    1. Server only

--- src/app/actions.ts
+++
@@ -1,5 +0,0 @@
-'use server'
-
-export const saveDate = async (date: Date): Promise<void> => {
-    console.log('saveDate', date);
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
-import {DateView} from "@/app/DateView";
-import {format} from "date-fns";
-
 export default function Home() {
-
     const date = new Date();
 
     return (
         <div className="flex flex-col w-[600px] mx-auto mt-12 gap-4">
-            <p>Server: {format(date, 'yyyy-MM-dd HH:mm')}</p>
-
-            <DateView serverDate={date}/>
+            <p>Server: {date.toLocaleString()}</p>
         </div>
     )
 }
