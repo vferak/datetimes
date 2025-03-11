85ed4d7115108961bccaf6cdcb6ad56d5bc0a72b
Vojtěch Ferák <vferak@gmail.com>
2025-03-11 22:12:45 +0100

    5. EuropePragueDate

--- src/app/DateView.tsx
+++ src/app/DateView.tsx
@@ -4,7 +4,7 @@ import {useState} from "react";
 import {DateTimePicker} from '@mui/x-date-pickers';
 import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
 import {AdapterDateFns} from '@mui/x-date-pickers/AdapterDateFnsV3'
-import {DATETIME_FORMAT, format} from "@/app/date";
+import {DATETIME_FORMAT, EuropePragueDate, format} from "@/app/date";
 import { cs } from 'date-fns/locale/cs';
 import {saveDate} from "@/app/actions";
 import {format as formatDateFns} from "date-fns";
@@ -23,7 +23,7 @@ export function DateView({serverDate}: { serverDate: Date }) {
         <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={cs}>
             <p>Client: {formatDateFns(clientDate, DATETIME_FORMAT)}</p>
             <p>Server on Client: {format(date)}</p>
-            <DateTimePicker value={date} ampm={false} onChange={async (newValue) => {
+            <DateTimePicker value={new EuropePragueDate(date)} ampm={false} onChange={async (newValue) => {
                 if (newValue !== null) {
                     await updateDate(newValue);
                 }
--- src/app/date.ts
+++ src/app/date.ts
@@ -1,5 +1,5 @@
 import { format as dateFnsFormat } from "date-fns";
-import { tz } from "@date-fns/tz";
+import { tz, TZDate } from "@date-fns/tz";
 
 export const DATETIME_FORMAT = 'yyyy-MM-dd HH:mm' as const
 
@@ -7,4 +7,10 @@ export const TIMEZONE_EUROPE_PRAGUE = 'Europe/London' as const
 
 export const format = (date: Date) => {
     return dateFnsFormat(date, DATETIME_FORMAT, { in: tz(TIMEZONE_EUROPE_PRAGUE) });
+}
+
+export class EuropePragueDate extends TZDate {
+    constructor(date?: Date) {
+        super(date || new Date(), TIMEZONE_EUROPE_PRAGUE)
+    }
 }
